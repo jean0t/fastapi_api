1f444943bb8d5c7e14a5a8f68f8cce40837c4790
jean0t <joaomouryf@hotmail.com>
2024-04-20 21:41:33 -0300

    environment configured

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="SimpleAPI")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True) 
